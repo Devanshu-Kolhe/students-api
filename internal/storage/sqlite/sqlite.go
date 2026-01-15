23a78f3067fcfc592d71593b673c9e8dd555d41f
Devanshu-Kolhe <devkolhe0410@gmail.com>
2026-01-16 04:41:39 +0530

    add students list

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Devanshu-Kolhe/students-api/internal/config"
 	"github.com/Devanshu-Kolhe/students-api/internal/types"
+	// "github.com/Devanshu-Kolhe/students-api/internal/utils/response"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -78,4 +79,33 @@ func (s *Sqlite) GetStudentById(id int64) (types.Student, error) {
 	}
 
 	return student, nil
+}
+
+
+func (s *Sqlite) GetStudents()([]types.Student, error){
+	stmt,err := s.Db.Prepare("SELECT id, name, email, age FROM students")
+	if err!=nil{
+		return nil,err
+	}
+	defer stmt.Close()
+
+	rows,err := stmt.Query()
+	if err!=nil{
+		return nil,err
+	}
+	defer rows.Close()
+
+	var students []types.Student
+
+	for rows.Next(){
+		var student types.Student
+
+		err := rows.Scan(&student.Id, &student.Name, &student.Email, &student.Age)
+		if err!=nil{
+			return nil,err
+		}
+
+		students = append(students, student)
+	}
+	return students, nil
 }
--- internal/types/types.go
+++ internal/types/types.go
@@ -3,6 +3,6 @@ package types
 type Student struct {
 	Id    int
 	Name  string `json:"name" validate:"required"`
-	Email string `json:"email"validate:"required"`
-	Age   int    `json:"age"validate:"required"`
+	Email string `json:"email" validate:"required"`
+	Age   int    `json:"age" validate:"required"`
 }
